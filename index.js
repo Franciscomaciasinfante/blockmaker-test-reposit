5181277e4342c61b0a76ffa76ee839c9b7a9b111
Franciscomaciasinfante <129832896+Franciscomaciasinfante@users.noreply.github.com>
2023-10-27 16:07:13 +0200

    Añadir ejercicio

---
+++ index.js
@@ -0,0 +1,20 @@
+// Ejercicio 1: Variables y Tipos de Datos
+
+const nombre = 'Juan'
+const edad = 25
+const esEstudiante = true
+const mensaje = 'Hola, mi nombre es ' + nombre + ' y tengo ' + edad + ' años. Soy estudiante: ' + esEstudiante + '.'
+
+const num1 = 10
+const num2 = 5
+
+const suma = num1 + num2
+const resta = num1 - num2
+const multiplicacion = num1 * num2
+const division = num1 / num2
+
+console.log(mensaje)
+console.log('Suma:', suma)
+console.log('Resta:', resta)
+console.log('Multiplicación:', multiplicacion)
+console.log('División:', division)
